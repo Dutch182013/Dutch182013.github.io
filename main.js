d25c0a1d00e6a7e2e9aa65750d40b5df1a6569e2
Dutch182013 <96445963+Dutch182013@users.noreply.github.com>
2023-11-21 11:30:54 +0700

    Add files via upload

--- main.js
+++ main.js
@@ -1,6 +1,24 @@
+let textprofile = document.getElementById("textprofile");
+
+onrun = (run, fps)=>{
+    window.onload = ()=>{
+        setInterval(run, 1000 / fps)
+    }
+}
+
+random = (max, min)=>{
+    return Math.random() * max + min
+}
+
+updateprofile = ()=>{
+    textprofile.innerHTML = `<p style="color: rgba(${random(255,1)},${random(255,1)},${random(255,1)},255);">Profile</p>`
+}
+
 let a1onclick = () => {
     let number="number: 0825781902\n"
     let fb="FB: Dutch Cpuy\n"
     let yt="Youtube: curious person"
     alert(number+fb+yt)
 }
+
+onrun(updateprofile,15)
